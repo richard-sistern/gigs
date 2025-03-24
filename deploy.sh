e3860a5c32321b1077e0b0e906932b867b8313b1
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2025-03-24 15:22:14 +0000

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-magick mogrify -path src/img/gigs/thumbs -resize 300x300 -quality 100 -strip src/img/gigs/*.jpg
-
+magick mogrify -path src/img/gigs/thumbs -resize 300x300 -quality 100 -strip original-images/*.jpg
+# npm run-script serve
 # git subtree push --prefix _site origin gh-pages
